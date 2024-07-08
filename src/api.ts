a08caef5fb284b40baf404acd0b04644cffe173f
Francesco Fagnani <francesco@eolasmedical.com>
2024-07-08 08:40:40 +0200

    feat(custom domain): added custom domain

--- src/api.ts
+++ src/api.ts
@@ -7,8 +7,7 @@ import {
   User,
 } from './types';
 
-export const API_BASE_URL =
-  'https://s7zb7mc40b.execute-api.eu-central-1.amazonaws.com/dev/v1';
+export const API_BASE_URL = 'https://api.borrow-lend.com/borrowlend/v1';
 
 const fetchWrapper = async <T>(url: string, options: RequestInit = {}) => {
   // Request interceptor
